42d5d8ab71c1dd850c85165449952c420bc85812
Sascha <saschamarijnissen2@gmail.com>
2024-10-21 09:00:20 +0200

    opdr 1

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,15 +1,15 @@
 // Select the necessary DOM elements
-const heading = document.getElementById('');
-const textInput = document.getElementById('');
-const changeTextButton = document.getElementById('');
+const heading = document.getElementById('mainHeading');
+const textInput = document.getElementById('textInput');
+const changeTextButton = document.getElementById('changeTextButton');
 
 // Add an event listener to the button
 // the first argument is the event type
 // the second argument is a callback function that will be called when the event is triggered
-changeTextButton.addEventListener('click', () => {});
+changeTextButton.addEventListener('click', changeHeadingText);
 
 // Function to change the heading text
 function changeHeadingText() {
-    const newText = '';
-    heading.textContent = ''; // Update the heading with the new text
+    const newText = textInput.value;
+    heading.textContent = newText; // Update the heading with the new text
 }
